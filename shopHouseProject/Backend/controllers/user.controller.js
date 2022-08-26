806a9c319c354647aaa1422e96ad9f1dbbd351c1
Janith Gamage <janithgamage1.ed@gmail.com>
2022-08-26 07:37:24 +0530

    fix: update desc: user REST API's Updated

---
+++ shopHouseProject/Backend/controllers/user.controller.js
@@ -0,0 +1,124 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+import User from '../models/user.model.js'
+
+export const signIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) return res.status(404).json({ message: "User doesn't exist" })
+
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
+
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid Credentials" })
+
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" })
+
+        res.status(200).json({ result: existingUser, token })
+
+
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
+}
+
+export const signUp = async (req, res) => {
+    const {
+        email,
+        password,
+        confirmPassword,
+        type,
+        firstName,
+        lastName,
+        userQNumber,
+        userContactNumber,
+        addressNumber,
+        addressLine1,
+        addressLine2,
+        userFaculty,
+        userField,
+        userSpecializedCriteria
+    } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) return res.status(400).json({ message: "User already exist" })
+
+        if (password !== confirmPassword) return res.status(400).json({ message: "Password don't match" })
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        const result = await User.create(
+            {
+                email,
+                password: hashedPassword,
+                type,
+                userDetails: {
+                    userQNumber,
+                    userEmail: email,
+                    userName: `${firstName} ${lastName}`,
+                    userContactNumber,
+                    userAddress: `${addressNumber},${addressLine1},${addressLine2}`,
+                    userFaculty,
+                    userField,
+                    userSpecializedCriteria,
+                    userType: type
+                }
+            }
+        )
+
+        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h" })
+
+        res.status(200).json({ result: result, token })
+
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
+}
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+
+        res.status(200);
+        res.json(users);
+
+    } catch (error) {
+        console.log(error)
+
+        res.status(404);
+        res.json({ message: error.message })
+    }
+}
+
+export const getUser = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(id);
+
+        res.status(200);
+        res.json(user);
+    } catch (error) {
+        res.status(404);
+        res.json({ "message": error.message });
+    }
+}
+
+export const getUserAccordingToType = async (req, res) => {
+    const { userType } = req.body;
+
+    try {
+        const users = await User.find({ "type": { $in: userType } })
+
+        res.status(200);
+        res.json(users);
+    } catch (error) {
+        res.status(404);
+        res.json({ "message": error.message });
+    }
+}
--- shopHouseProject/Backend/server.js
+++ shopHouseProject/Backend/server.js
@@ -1,13 +1,14 @@
-import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
 
 dotenv.config()
 
 //import routes
 import tutorialRoutes from "./routes/tutorial.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/tutorial', tutorialRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.46vukap.mongodb.net/?retryWrites=true&w=majority`;
 
