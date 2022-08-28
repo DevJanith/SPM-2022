34fda74e6662103b69d1a805ccab97290a93e434
Janith Gamage <janithgamage1.ed@gmail.com>
2022-08-28 17:38:18 +0530

    fix: update desc : swagger doc updated

--- shopHouseProject/Backend/server.js
+++ shopHouseProject/Backend/server.js
@@ -3,21 +3,48 @@ import cors from 'cors';
 import dotenv from "dotenv";
 import express from 'express';
 import mongoose from 'mongoose';
-
-dotenv.config()
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from "swagger-ui-express"
 
 //import routes
 import tutorialRoutes from "./routes/tutorial.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/tutorial', tutorialRoutes);
-app.use('/user', userRoutes);
+//swagger
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            "title": "Shop House REST API",
+            "description": "A REST API built with Express and MongoDB. This API provides Shop House Rest API's.",
+            "version": "0.0.1"
+        },
+        servers: [
+            {
+                "url": "http://localhost:8000/",
+                "description": "Shop House API's"
+            }
+        ]
+    },
+    apis: ["./routes/*.js"]
+}
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/shop-house/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.get('/shop-house/swagger-doc', (req, res) => {
+    res.send(swaggerDocs)
+})
+//end
+
+app.use('/shop-house/tutorial', tutorialRoutes);
+app.use('/shop-house/user', userRoutes);
 
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.46vukap.mongodb.net/?retryWrites=true&w=majority`;
 
