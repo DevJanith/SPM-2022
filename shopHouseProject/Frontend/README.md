b0edfc39eda81b0dcdf099addf2593d32727ca26
Janith Gamage <janithgamage1.ed@gmail.com>
2022-09-02 23:55:19 +0530

    fix: update

--- shopHouseProject/Frontend/README.md
+++
@@ -1,2 +0,0 @@
-#  Shop House Front end
- 
--- shopHouseProject/Frontend/src/pages/FeedBackManagement/FeedBack.container.js
+++
@@ -1,8 +0,0 @@
-import { FeedBackView } from "./FeedBack.view"
-
-export default function FeedBackContainer() {
-    const props = {
-    }
-
-    return <FeedBackView  {...props} />
-}
