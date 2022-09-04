bfd4ac0662e9be483f1542f960024db2e44c94e7
supundileepa00 <paranagamasupun@gmail.com>
2022-09-04 21:07:07 +0530

    added

--- referenceProject/backend/config/db.config.js
+++ referenceProject/backend/config/db.config.js
@@ -1,13 +1,13 @@
 export const dbConfig = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "151217",
-    DB: "farmerPortal",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "farmerPortal",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
