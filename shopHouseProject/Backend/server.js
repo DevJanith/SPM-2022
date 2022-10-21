34fd20c5e767dc49555fe1caf554daac12e87228
Janith Gamage <janithgamage1.ed@gmail.com>
2022-10-21 13:14:36 +0530

    fix: update desc: project backend

--- shopHouseProject/Backend/server.js
+++ shopHouseProject/Backend/server.js
@@ -2,10 +2,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
-import { Stripe } from "stripe";
-import uuid from 'react-uuid';
-
+import mongoose from "mongoose"; 
 
 //import routes
 import feedbackRoutes from "./routes/feedback.routes.js";
@@ -13,12 +10,10 @@ import itemRoutes from "./routes/item.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import tutorialRoutes from "./routes/tutorial.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
 
-const app = express();
 dotenv.config();
-
-//stripe implementation
-const stripe = Stripe("sk_test_51L45q8LRHo7ESm3WbBWnMoykN3km7eS8OjPASjNE3lViFXubXmGMJMVTOL3whVoL21AghocUo5rAOZWNR7iy91qU00oN1kaRVy")
+const app = express(); 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -56,47 +51,18 @@ app.use(cors());
 //end
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Farm Portal Server" });
-});
-
-app.post("/payment", (req, res) => {
-  const { product, token } = req.body;
-  console.log("Product", product);
-  console.log("Price", product.price);
-  // To avoid duplication for payments
-  const idImpotencyKey = uuid();
-  return stripe.customers.create({
-    email: token.email,
-    source: token.id
-  })
-    .then(customer => {
-      stripe.charger.create({
-        amount: product.price * 100,
-        currency: 'usd',
-        customer: customer.id,
-        receipt_email: token.email,
-        description: `purchase of $(product.name)`,
-        shipping: {
-          name: token.card.name,
-          address: {
-            country: token.card.address_country
-          }
-        }
-      }, { idImpotencyKey })
-    })
-    .then(result => res.status(200).json(result))
-    .catch(err => { console.log(err) })
-})
-
+  res.json({ message: "Welcome to Shop House Server" });
+}); 
 
 app.use("/shop-house/tutorial", tutorialRoutes);
 app.use("/shop-house/user", userRoutes);
 app.use("/shop-house/feedback", feedbackRoutes);
 app.use("/shop-house/item", itemRoutes);
 app.use("/shop-house/product", productRoutes);
+app.use("/shop-house/payment", paymentRoutes);
+
 
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.46vukap.mongodb.net/?retryWrites=true&w=majority`;
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -105,7 +71,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server Running on port : ${PORT}`));
+    app.listen(PORT, () => console.log(`Server Running on port :http://localhost:${PORT}`));
   })
   .catch((error) => {
     console.log(error.message);
