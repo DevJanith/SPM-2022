0f3bcb5aba7764e5856a9eafdca35b25d7ee8bbf
Himasha Ranaweera <52014041+Sumudu-Himasha-Ranaweera@users.noreply.github.com>
2022-10-22 07:38:18 +0530

    updare:shop management

--- shopHouseProject/Frontend/src/pages/Project/ProductApproveManagement/Products.js
+++ shopHouseProject/Frontend/src/pages/Project/ProductApproveManagement/Products.js
@@ -1,7 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 import {
-    Button, Card, Checkbox, Container, Stack, Table, TableBody,
-    TableCell, TableContainer,
+    Button, Card, Grid, Checkbox, Container, Stack, Table, TableBody,
+    TableCell, TableContainer, Chip,
     TablePagination, TableRow
 } from '@mui/material';
 import Alert from '@mui/material/Alert';
@@ -14,6 +14,11 @@ import Page from '../../../components/Page';
 import Scrollbar from '../../../components/Scrollbar';
 import SearchNotFound from '../../../components/SearchNotFound';
 import { ItemListHead, ItemListToolbar, ItemMoreMenu, ProductMoreMenu } from '../../../sections/@dashboard/item';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { deleteProduct, fetchOneProduct } from '../../../api';
+import { useEffect } from "react";
 
 
 const TABLE_HEAD = [
@@ -72,17 +77,28 @@ export default function Item(props) {
     const dispatch = useDispatch();
 
     const [page, setPage] = useState(0);
-
     const [order, setOrder] = useState('asc');
-
     const [selected, setSelected] = useState([]);
-
     const [orderBy, setOrderBy] = useState('name');
-
     const [filterName, setFilterName] = useState('');
-
     const [rowsPerPage, setRowsPerPage] = useState(5);
+    
+    const [successDeleteData, setSuccessDeleteData] = useState()
+    const [errorDeleteData, setErrorDeleteData] = useState()
+    const [isDeleteSuccess, setIsDeleteSuccess] = useState(false)
+    const [isDeletePending, setIsDeletePending] = useState(false)
+    const [isDeleteError, setIsDeleteError] = useState(false)
 
+    const [successData, setSuccessData] = useState()
+    const [errorData, setErrorData] = useState()
+    const [isSuccess, setIsSuccess] = useState(false)
+    const [isPending, setIsPending] = useState(false)
+    const [isError, setIsError] = useState(false)
+    
+    const [Items, setUsers] = useState({
+        items: []
+    })
+    
     const handleRequestSort = (event, property) => {
         const isAsc = orderBy === property && order === 'asc';
         setOrder(isAsc ? 'desc' : 'asc');
@@ -179,6 +195,50 @@ export default function Item(props) {
         dispatch(createProduct(data));
     }
 
+    /* Fetch Items */
+    // const getUsers = async (values) => {
+    //     console.log(values);
+    //     setIsPending(true)
+
+    //     await dispatch(
+    //         fetchOneProduct()
+    //             .then((response) => {
+    //                 setSuccessData(response.data)
+    //                 setUsers({ items: response.data })
+    //                 setIsPending(false)
+    //                 setIsSuccess(true)
+    //             })
+    //             .catch((errors) => {
+    //                 setErrorData(errors.response)
+    //                 setIsPending(false)
+    //                 setIsError(true)
+    //             }))
+    // }
+
+
+    // const deleteUser = async (values) => {
+    //     console.log(values);
+    //     setIsPending(true)
+
+    //     await dispatch(
+    //         deleteProduct(values)
+    //             .then((response) => {
+    //                 setSuccessDeleteData(response.data)
+    //                 setIsDeletePending(false)
+    //                 setIsDeleteSuccess(true)
+    //             })
+    //             .catch((errors) => {
+    //                 setErrorDeleteData(errors.response)
+    //                 setIsDeletePending(false)
+    //                 setIsDeleteError(true)
+    //             }))
+    // }
+
+    // useEffect(() => {
+    //     getUsers()
+    //     return
+    // }, [isDeleteSuccess])
+
     return (
         <Page title="Item">
             <Container>
@@ -224,7 +284,9 @@ export default function Item(props) {
 
                                                 <TableCell align="left">{name}</TableCell>
                                                 <TableCell align="left">{description}</TableCell>
-                                                <TableCell align="left">{qty}</TableCell>
+                                                <TableCell align="left">
+                                                    <Chip label={qty} variant="outlined" />
+                                                </TableCell>
                                                 <TableCell align="left">{price}</TableCell>
 
 
@@ -248,6 +310,15 @@ export default function Item(props) {
                                                     </Button>
                                                 </TableCell>
 
+                                                <TableCell align="left">
+                                                <Grid item md={3} marginTop="15px">
+                                                        <Tooltip title="Delete">
+                                                            <IconButton aria-label="delete" size="large" style={{ border: "1px solid #c0c0c0", borderRadius: "10%" }} /*onClick={() => deleteUser(_id)}*/>
+                                                                <DeleteIcon color='error' />
+                                                            </IconButton>
+                                                        </Tooltip>
+                                                </Grid>
+                                                </TableCell>
 
                                                 <TableCell align="right">
                                                     <ItemMoreMenu
@@ -266,6 +337,7 @@ export default function Item(props) {
                                             </TableRow>
                                         );
                                     })}
+                                    
                                     {emptyRows > 0 && (
                                         <TableRow style={{ height: 53 * emptyRows }}>
                                             <TableCell colSpan={6} />
